b231e9914c4cd9d478b479d0292dbb7cd2a831e8
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-02-02 23:36:49 -0500

    DNA CodeWars!!

--- CodeWars.js
+++ CodeWars.js
@@ -138,3 +138,17 @@
 // function friend(friends) {
 //     return friends.filter(name => name.length === 4);
 //   }
+
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// function dnaStrand(dna) {
+//     const complement = {
+//         'A': 'T',
+//         'T': 'A',
+//         'C': 'G',
+//         'G': 'C'
+//     };
+//     return dna.split('').map(nucleotide => complement[nucleotide]).join('');
+// }
