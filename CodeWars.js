46367a3336ee2e41e0549c1bd7ec156e6c31071e
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-02-05 23:55:03 -0500

    the 7s are getting easier!

--- CodeWars.js
+++ CodeWars.js
@@ -166,3 +166,9 @@
 // function rowSumOddNumbers(n) {
 //     return n * n * n
 // }
+
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// function reverseList(list) {
+//return list.reverse()
+//}
