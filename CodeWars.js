512aa6c4335b13b3d764f6303f4e4490d2bbdfaf
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-02-08 23:54:14 -0500

    Late night problem solving!

--- CodeWars.js
+++ CodeWars.js
@@ -178,4 +178,15 @@
 // function filter_list(l) {
 //     return l.filter(item => typeof item === 'number');
 //   }
-  
+
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase.
+
+// String.prototype.toAlternatingCase = function () {
+//     return this.split('').map(char => {
+//       if (char === char.toUpperCase()) {
+//         return char.toLowerCase();
+//       } else {
+//         return char.toUpperCase();
+//       }
+//     }).join('');
+//   }
