55deacdc69df783bb6ff373f4b740e59f19bc132
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-02-09 22:40:19 -0500

    grinding codewars!

--- CodeWars.js
+++ CodeWars.js
@@ -190,3 +190,9 @@
 //       }
 //     }).join('');
 //   }
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// function removeUrlAnchor(url){
+//     return url.split('#')[0]
+//   }
