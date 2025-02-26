986fdce7d5afb211b78d071702ceec216397fd49
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-02-25 23:17:52 -0500

    getting back to coding

--- CodeWars.js
+++ CodeWars.js
@@ -227,3 +227,9 @@
 //       return false
 //     }
 //   }
+
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// function getChar(c){
+//   return String.fromCharCode(c)
+// }
