31793d4810c22c5dfa2dcdced5c0b0d77bfac7fb
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-03-06 23:46:19 -0500

    Array training!

--- CodeWars.js
+++ CodeWars.js
@@ -291,3 +291,15 @@
 // var websites = [];
 // websites.push("codewars");
 // console.log(websites);
+
+// Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+
+// var capitals = function (word) {
+// 	let indices = [];
+//     for (let i = 0; i < word.length; i++) {
+//         if (word[i] >= 'A' && word[i] <= 'Z') {
+//             indices.push(i);
+//         }
+//     }
+//     return indices;
+// };
