de3562f42d09b54194dbcffa8ddb43b96b109e65
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-06-02 19:04:35 -0400

    Semantical solution for sure

--- CodeWars.js
+++ CodeWars.js
@@ -424,3 +424,10 @@
 //     return on - wait
 //   }
 // }
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// function highAndLow(numbers){
+//   numbers = numbers.split(' ');
+//   return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+// }
