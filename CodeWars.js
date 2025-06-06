b853b08afcd16a5341b384c30694fb3251de7278
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-06-05 23:38:00 -0400

    high and low numbers

--- CodeWars.js
+++ CodeWars.js
@@ -447,3 +447,9 @@
 // function removeChar(str) {
 //   return str.slice(1, -1);
 // }
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// function descendingOrder(n){
+//   return parseInt(String(n).split('').sort().reverse().join(''))
+// }
