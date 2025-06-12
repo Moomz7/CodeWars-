117e4eb644516f89c16715b2d73e19afd3e763cc
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-06-11 23:56:39 -0400

    searching for numbers

--- CodeWars.js
+++ CodeWars.js
@@ -463,3 +463,10 @@
 // //   //your code here
 // //   return x*50+6;
 // // }
+
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// function findUniq(arr) {
+//   arr.sort((a,b)=>a-b);
+//   return arr[0]==arr[1]?arr.pop():arr[0]
+// }
