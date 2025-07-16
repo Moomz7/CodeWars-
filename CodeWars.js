047903d2232ea778bf5f2a26cc52993cf9780d23
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-07-15 22:56:59 -0400

    Javascript practice pt 16

--- CodeWars.js
+++ CodeWars.js
@@ -627,3 +627,23 @@
 // The second mission: Traversal arr, count the number of elements which are smaller than n and return it.
 
 // howManySmaller=(arr,n)=>arr.filter(a=>a.toFixed(2)<n).length
+
+// Task
+// Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+// The first mission: Traversing arr, find the shortest string length.
+
+// The second mission: Traversing arr again, intercept all strings to the shortest string length(Start from index0). you can use one of slice() substring() or substr() do it. return the result after finished the work.
+
+// function cutIt(arr) {
+//   var smallest = arr[0].length;
+//   for(var i = 0; i < arr.length; i++) {
+//     if(arr[i].length < smallest) {
+//       smallest = arr[i].length;
+//     }
+//   }
+//   var map = arr.map(function(x) {
+//     return x.slice(0, smallest);
+//   });
+//   return map;
+// };
