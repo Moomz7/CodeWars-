d73dc98f4b908c93c9264a1f65dbea4b66b2a514
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-07-17 23:53:44 -0400

    Javascript practice pt 18

--- CodeWars.js
+++ CodeWars.js
@@ -661,3 +661,15 @@
 //   if (firstIndex === -1) {return -1}
 //   else {return lastIndex-firstIndex}  
 // }
+
+// Task
+// Implement a function which accepts 2 arguments: string and separator.
+
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+// function splitAndMerge(string, separator) {
+//   return string.split('')
+//         .join(separator + '')
+//         .split(separator + ' ' + separator)
+//         .join(' ');
+// }
