28cb0ff9eda5ca44588f37892bc7a4bd4eabfbfb
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-07-21 22:01:29 -0400

    Javascript practice pt 19

--- CodeWars.js
+++ CodeWars.js
@@ -673,3 +673,20 @@
 //         .split(separator + ' ' + separator)
 //         .join(' ');
 // }
+
+// Task
+// Coding in function alienLanguage, function accept 1 parameter:str. str is a sentence.
+
+// We translate the sentence into an alien language according to the following rules:
+
+// Each word in the sentence is separated by spaces. The last letter of each word in the sentence turns to lowercase, and the other letters are capitalized. Looks very strange? Because this is the form of alien language ;-)
+
+// function alienLanguage(str){
+//   var words = str.toUpperCase().split(" ");
+//   for (var i = 0; i < words.length; i++) {
+//     var precedingChars = words[i].slice(0, -1);
+//     var lastChar = words[i].slice(-1).toLowerCase();  
+//     words[i] = precedingChars + lastChar;
+//   }
+//   return words.join(" ");
+// }
