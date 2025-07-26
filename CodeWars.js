a9d9856c0d1bc5f865d0d73ed31a055c7d8197f9
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-07-26 19:17:08 -0400

    square sum function

--- CodeWars.js
+++ CodeWars.js
@@ -727,3 +727,9 @@
 //   }
 //   return null
 // }
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// function squareSum(numbers){
+//   return numbers.reduce((sum,num) => sum + (num * num), 0);
+// }
