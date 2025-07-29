2632b5bf928e1e0a4ae8dbe9e831df9a52304418
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-07-28 23:33:14 -0400

    array practice

--- CodeWars.js
+++ CodeWars.js
@@ -733,3 +733,14 @@
 // function squareSum(numbers){
 //   return numbers.reduce((sum,num) => sum + (num * num), 0);
 // }
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// function between(a, b) {
+//   // your code here
+//   arr = []
+//   for(i = a;i <= b; i++){ 
+//       arr.push(i)
+//   }
+//   return arr
+// }
