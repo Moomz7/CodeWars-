4cd6d500d632620692755032c72f48130d11a1d6
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-08-07 22:29:48 -0400

    new array!

--- CodeWars.js
+++ CodeWars.js
@@ -824,3 +824,14 @@
     
 //     return Math.max(...list)
 // }
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+// const reverseSeq = n => {
+// let arr = [];
+//   for (let i=n; i>0; i--) {
+//     arr.push(i);
+//     } return arr;
+// };
