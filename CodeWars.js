9ad66fac2559a59b41b3c57e1db70d72cdeb5e03
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-08-11 20:07:31 -0400

    repeatrepeat

--- CodeWars.js
+++ CodeWars.js
@@ -835,3 +835,9 @@
 //     arr.push(i);
 //     } return arr;
 // };
+
+// Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// function repeatStr (n, s) {
+//   return s.repeat(n)
+// }
