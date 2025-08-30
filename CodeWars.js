9f888a87a080d0c32e0a9e1005410922007a28d2
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-08-29 23:59:50 -0400

    remove duplicates

--- CodeWars.js
+++ CodeWars.js
@@ -975,3 +975,11 @@
 // function sayHello( name, city, state ) {
 // return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
 // }
+
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// function distinct(a) {
+//   return [...new Set(a)];
+// }
