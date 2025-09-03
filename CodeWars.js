54487bb11091d83a84ddb843e7bda35ba7fe653b
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-09-02 23:59:04 -0400

    converting to binary

--- CodeWars.js
+++ CodeWars.js
@@ -1013,3 +1013,7 @@
 //   return 'Hello, World!'
 //   }
 // }
+
+// Given a non-negative integer b, write a function which returns an integer d such that the binary representation of b is the same as the decimal representation of d.
+
+// let toBinary = n => +n.toString(2)
