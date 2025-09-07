1de401c7430e28c0c7816d47d4cc12d687456bed
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-09-06 23:58:44 -0400

    finding 1st element

--- CodeWars.js
+++ CodeWars.js
@@ -1038,3 +1038,9 @@
     
 //   return result
 // }
+
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+// function take(arr, n) {
+//   return arr.slice(0, n);
+// }
