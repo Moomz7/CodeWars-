1a0ce8f0a62343dc6a5c882a4b2d21801b164004
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-09-10 23:18:28 -0400

    whats the remainder

--- CodeWars.js
+++ CodeWars.js
@@ -1098,3 +1098,14 @@
 //      return "You were born this very year!";
 //    }
 // }
+
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+// Division by zero should return NaN.
+
+// function remainder(a, b){
+//   if(b>a)
+//     return b%a;
+//   else
+//     return a%b;
+// }
