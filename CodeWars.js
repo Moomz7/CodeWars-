ccaacae7e19ac150c26c7edfc96440e3c53ea9ff
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-09-11 22:51:45 -0400

    finding multiples

--- CodeWars.js
+++ CodeWars.js
@@ -1109,3 +1109,19 @@
 //   else
 //     return a%b;
 // }
+
+// Find the sum of all multiples of n below m
+
+// Keep in Mind
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+
+// function sumMul(n,m){
+//   if (n >= m) return "INVALID";
+
+// var sum = 0;
+//   for (var i = n; i < m; i+=n) {
+//     sum += i;
+//   }
+//   return sum;
+// }
