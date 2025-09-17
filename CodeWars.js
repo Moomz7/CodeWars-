92bff2ed08ed1312155ee64ef217b04d99f7a0bf
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-09-17 09:04:53 -0400

    Act pt 2

--- CodeWars.js
+++ CodeWars.js
@@ -387,3 +387,16 @@
 // function binToDec(bin){
 //   return parseInt(bin,2);
 // }
+
+// Task
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+// Note: Input will either be a positive integer (or a string for untyped languages).
+
+// function apple(x){
+// if(x*x > 1000){
+//   return "It's hotter than the sun!!"
+// }else{
+//   return "Help yourself to a honeycomb Yorkie for the glovebox."
+// }
+// }
