d8bc72559b8e8ac82eb25da0e7e3a159aa844fac
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-09-16 23:57:58 -0400

    binary to decimal

--- CodeWars.js
+++ CodeWars.js
@@ -381,3 +381,9 @@
 //       }
 //       return val;
 //     }
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// function binToDec(bin){
+//   return parseInt(bin,2);
+// }
