d55766600ca792435b7de1ae3a124203b8035eda
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-09-18 22:48:02 -0400

    3rd side of the triangle

--- CodeWars.js
+++ CodeWars.js
@@ -400,3 +400,13 @@
 //   return "Help yourself to a honeycomb Yorkie for the glovebox."
 // }
 // }
+
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// function otherAngle(a, b) {
+//   return 180-(a+b);
+// }
