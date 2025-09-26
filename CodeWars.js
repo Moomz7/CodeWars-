e4ceee11875c4305b59e6e69e2524f21101d9c00
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-09-25 22:10:09 -0400

    simple array method

--- CodeWars.js
+++ CodeWars.js
@@ -462,3 +462,15 @@
 //     return "Congratulations! You're going to have a son."
 //   }
 // }
+
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// function printArray(array){
+//   return array.join(",")
+// }
