92ce2cbe296dcfd0b837a782b7e30ff87ef29cfd
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-10-02 21:57:46 -0400

    even or odd?

--- CodeWars.js
+++ CodeWars.js
@@ -564,3 +564,20 @@
 //     return "Nine"
 //   }
 // }
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// function oddOrEven(array) {
+//   let result = 0 
+//   for(let i = 0; i < array.length; i++)
+//     result += array[i]
+//   if(result%2 === 0){
+//     return "even"
+//   }else if(result%0 != 0){
+//     return "odd"
+//   }
+// }
