07a389da0af34dc765268eaeb6bbc405842a6b45
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-10-29 22:44:57 -0400

    naming properly

--- CodeWars.js
+++ CodeWars.js
@@ -255,3 +255,9 @@
 //   // Return the result
 //   return area;
 // }
+
+// Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// function combineNames(firstName, lastName){
+//   return `${firstName}` + " " + `${lastName}`
+// }
