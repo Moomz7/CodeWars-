b247da8b4e42f6f9dff216ff3b1bb99a2a718ef5
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-11-02 23:37:15 -0500

    or logic

--- CodeWars.js
+++ CodeWars.js
@@ -279,3 +279,17 @@
 //     return a/b
 //   }
 // }
+
+// Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
+
+// function xor(a, b) {
+//   if(a == b){
+//     return false
+//   }else if(a == true && b == false){
+//     return true
+//   }else if(a == false && b == true){
+//     return true
+//   }else{
+//     return false
+//   }
+// }
