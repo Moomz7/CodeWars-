19773578e5b9861b449e7dd27dd7a9c6ee1842b6
Moumie Dibinga <moumie.dibinga@gmail.com>
2025-11-22 23:18:19 -0500

    string reversal

--- CodeWars.js
+++ CodeWars.js
@@ -530,3 +530,11 @@
 //     return 'Who ate the last cookie? It was the dog!';
 //   }
 // }
+
+// You need to write a function that reverses the words in a given string. Words are always separated by a single space.
+
+// As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+// function reverse(string){
+//   return string.split(" ").reverse(string).join(" ")
+// }
